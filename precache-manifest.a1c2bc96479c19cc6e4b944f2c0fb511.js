02197274d40bb487a48b6325a98107fbd7e61d5f
kanchana123 <kanchan.nannavare@yotes.collegeofidaho.edu>
2019-03-21 17:07:13 +0530

    Updates

---
+++ precache-manifest.a1c2bc96479c19cc6e4b944f2c0fb511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69393802b4b95d79fecc",
+    "url": "/beer-bank-turing-front-end-challenge/static/css/main.688e974d.chunk.css"
+  },
+  {
+    "revision": "69393802b4b95d79fecc",
+    "url": "/beer-bank-turing-front-end-challenge/static/js/main.69393802.chunk.js"
+  },
+  {
+    "revision": "6bbf3f2517236e042443",
+    "url": "/beer-bank-turing-front-end-challenge/static/js/runtime~main.6bbf3f25.js"
+  },
+  {
+    "revision": "d93479244458985b291f",
+    "url": "/beer-bank-turing-front-end-challenge/static/js/2.d9347924.chunk.js"
+  },
+  {
+    "revision": "7a432ae87e734e216bd5afae96cddfc2",
+    "url": "/beer-bank-turing-front-end-challenge/index.html"
+  }
+];
